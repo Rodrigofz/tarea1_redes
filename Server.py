591842a288a61eb5e2b70549ef66eff70b354486
Rodrigo Fuentes <rodrigo.fuentes.z@ug.uchile.cl>
2019-04-03 12:56:17 -0300

    Funcion que pone a correr el servidor

--- Server.py
+++ Server.py
@@ -1,55 +1,36 @@
 import socket
-
+import sys
  
+def main(**options):
+    puerto = options.get("puerto")
+    print(options.get("saludo"))
+    localIP     = "127.0.0.1"
+    localPort   = 20001
+    bufferSize  = 1024
 
-localIP     = "127.0.0.1"
+    msgFromServer       = "Hello UDP Client"
+    bytesToSend         = str.encode(msgFromServer)
 
-localPort   = 20001
+    # Create a datagram socket
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-bufferSize  = 1024
+    # Bind to address and ip
+    UDPServerSocket.bind((localIP, localPort))
+    print("UDP server up and listening")
 
- 
-
-msgFromServer       = "Hello UDP Client"
-
-bytesToSend         = str.encode(msgFromServer)
-
- 
-
-# Create a datagram socket
-
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
- 
-
-# Bind to address and ip
-
-UDPServerSocket.bind((localIP, localPort))
-
- 
-
-print("UDP server up and listening")
-
- 
-
-# Listen for incoming datagrams
-
-while(True):
-
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
-    message = bytesAddressPair[0]
-
-    address = bytesAddressPair[1]
-
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
+    # Listen for incoming datagrams
+    while(True):
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP  = "Client IP Address:{}".format(address)
     
-    print(clientMsg)
-    print(clientIP)
+        print(clientMsg)
+        print(clientIP)
 
-   
+        # Sending a reply to client
+        UDPServerSocket.sendto(bytesToSend, address)
 
-    # Sending a reply to client
-
-    UDPServerSocket.sendto(bytesToSend, address)
+if __name__ == "__main__":
+    main(puerto=sys.argv[1], saludo="holi")
